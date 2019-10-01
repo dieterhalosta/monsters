ef7305e48229046e2fd6803f89d7fc0b278ecb1f
Dieter Halosta <dieter.halosta@yahoo.com>
2019-10-01 14:57:31 +0300

    Updates

---
+++ precache-manifest.4a76988c4e417144adeec52c6be8dd67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f5f009a399fea35b3f11e5411f7ab0",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "09d7bfab3c9feabcd8f6",
+    "url": "/monsters/static/css/main.7f41a716.chunk.css"
+  },
+  {
+    "revision": "964ccf7ba0449dad8dff",
+    "url": "/monsters/static/js/2.a20b2da9.chunk.js"
+  },
+  {
+    "revision": "09d7bfab3c9feabcd8f6",
+    "url": "/monsters/static/js/main.edd50d51.chunk.js"
+  },
+  {
+    "revision": "a2665b738257676d17f2",
+    "url": "/monsters/static/js/runtime-main.9c1f6176.js"
+  }
+]);
